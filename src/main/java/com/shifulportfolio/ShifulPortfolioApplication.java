22633bdde65fdf8b59bb1512e0c97a84851c8033
arttinex03-svg <attinex03@gmail.com>
2026-07-19 03:16:28 +0600

    Initial commit

---
+++ src/main/java/com/shifulportfolio/ShifulPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.shifulportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifulPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShifulPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shifulportfolio/ShifulPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shifulportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifulPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
